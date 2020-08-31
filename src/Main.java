b16d1ef9e812c674aaee3867489f1b182d8a139e
brunaiis2013pro <brunaiis2013pro@gmail.com>
2020-08-30 23:37:38 -0400

    practica1:Agencia de viajes

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //Cliente
+        Cliente carlos = new Cliente(1,"Carlos","Perez","Vargas");
+        //Ciudad;
+        Ciudad cochabamba = new Ciudad(1,"Cochabamba");
+        Ciudad santaCruz = new Ciudad(2,"Santa Cruz");
+        Ciudad tarija = new Ciudad(3,"Tarija");
+
+        //Categoria
+        Categoria turista = new Categoria(1,"Turista");
+
+        //Aeropuertos
+        Aeropuerto jorgeWilstermann = new Aeropuerto("JW","Jorge Wilstermann",cochabamba);
+        Aeropuerto viruViru = new Aeropuerto("VV","Viru Viru",santaCruz);
+        Aeropuerto orielLea = new Aeropuerto("OL","Oriel Lea Plaza",tarija);
+
+
+        //Vuelos
+        List<Vuelo> vuelos = new ArrayList<>();
+        Vuelo cochabambaSantacruz = new Vuelo("BOA-158", Calendar.getInstance().getTime(),jorgeWilstermann,viruViru );
+        vuelos.add(cochabambaSantacruz);
+        Vuelo santaCruzTarija = new Vuelo("BOA-159",Calendar.getInstance().getTime(),viruViru,orielLea );
+        vuelos.add(santaCruzTarija);
+
+        //Billete
+        Billete billete = new Billete(1,Calendar.getInstance().getTime(), 650.5,carlos,cochabamba,tarija,turista,vuelos);
+        System.out.println("Billete");
+        System.out.println("=====");
+        System.out.println("Id: "+billete.getIdBillete());
+        System.out.println("Fecha: "+billete.getFechaEmision());
+        System.out.println("Precio Total: "+billete.getPrecioTotal());
+        System.out.println("Cliente: "+billete.getCliente().getNombre());
+        System.out.println("Origen: "+ billete.getOrigen().getNombre());
+        System.out.println("Destino: "+billete.getDestino().getNombre());
+        System.out.println("Categoria: "+billete.getCategoria().getNombre());
+        System.out.println("Vuelos: ");
+        for (Vuelo vuelo:billete.getVuelos()) {
+            System.out.println("Codigo vuelo: "+vuelo.getCodigoVuelo()+" | Salida: "+vuelo.getSalida()+" | Origen: "+vuelo.getOrigen().getNombre() + " | Destino:  "+vuelo.getDestino().getNombre());
+        }
+    }
+}
